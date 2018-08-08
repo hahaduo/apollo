cfdd95db9795ea2b88995dbdd04c54ee8182fd7e
lihongjun <lihongjun@zkyda.com>
2018-08-08 10:51:29 +0800

    修改dev_meta地址

--- apollo-docker/build.sh
+++ apollo-docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "==== starting to build config-service and admin-service ===="
-mvn clean package -DskipTests -pl apollo-configservice,apollo-adminservice -am -Dapollo_profile=github -Dspring_datasource_url=jdbc:mysql://localhost:3306/ApolloConfigDB?characterEncoding=utf8 -Dspring_datasource_username=root
+mvn clean package -DskipTests -pl apollo-configservice,apollo-adminservice -am -Dapollo_profile=github -Dspring_datasource_url=jdbc:mysql://39.104.102.91:3306/ApolloConfigDB?characterEncoding=utf8 -Dspring_datasource_username=root
 
 echo "==== starting to build portal ===="
-mvn clean package -DskipTests -pl apollo-portal -am -Dapollo_profile=github,auth -Dspring_datasource_url=jdbc:mysql://localhost:3306/ApolloPortalDB?characterEncoding=utf8 -Dspring_datasource_username=root -Ddev_meta=http://localhost:8080
+mvn clean package -DskipTests -pl apollo-portal -am -Dapollo_profile=github,auth -Dspring_datasource_url=jdbc:mysql://39.104.102.91:3306/ApolloPortalDB?characterEncoding=utf8 -Dspring_datasource_username=root -Ddev_meta=http://localhost:8080
--- scripts/build.sh
+++ scripts/build.sh
@@ -11,10 +11,10 @@ apollo_portal_db_username=root
 apollo_portal_db_password=123456
 
 # meta server url, different environments should have different meta server addresses
-dev_meta=http://192.168.31.192:8081
-fat_meta=http://192.168.31.192:8081
-uat_meta=http://192.168.31.192:8081
-pro_meta=http://192.168.31.192:8081
+dev_meta=http://172.31.36.107:8081
+fat_meta=http://172.31.36.107:8081
+uat_meta=http://172.31.36.107:8081
+pro_meta=http://172.31.36.107:8081
 
 META_SERVERS_OPTS="-Ddev_meta=$dev_meta -Dfat_meta=$fat_meta -Duat_meta=$uat_meta -Dpro_meta=$pro_meta"
 
@@ -27,13 +27,13 @@ cd ..
 # package config-service and admin-service
 echo "==== starting to build config-service and admin-service ===="
 
-mvn clean package -DskipTests -pl apollo-configservice,apollo-adminservice -am -Dapollo_profile=github -Dspring_datasource_url=$apollo_config_db_url -Dspring_datasource_username=$apollo_config_db_username -Dspring_datasource_password=$apollo_config_db_password -Dlogging.file=/opt/logs/apollo-assembly.log -Dip=192.168.31.192
+mvn clean package -DskipTests -pl apollo-configservice,apollo-adminservice -am -Dapollo_profile=github -Dspring_datasource_url=$apollo_config_db_url -Dspring_datasource_username=$apollo_config_db_username -Dspring_datasource_password=$apollo_config_db_password -Dlogging.file=/opt/logs/apollo-assembly.log -Dip=172.31.36.107
 
 echo "==== building config-service and admin-service finished ===="
 
 echo "==== starting to build portal ===="
 
-mvn clean package -DskipTests -pl apollo-portal -am -Dapollo_profile=github,auth -Dspring_datasource_url=$apollo_portal_db_url -Dspring_datasource_username=$apollo_portal_db_username -Dspring_datasource_password=$apollo_portal_db_password -Dlogging.file=/opt/logs/apollo-assembly.log $META_SERVERS_OPTS -Dip=192.168.31.192
+mvn clean package -DskipTests -pl apollo-portal -am -Dapollo_profile=github,auth -Dspring_datasource_url=$apollo_portal_db_url -Dspring_datasource_username=$apollo_portal_db_username -Dspring_datasource_password=$apollo_portal_db_password -Dlogging.file=/opt/logs/apollo-assembly.log $META_SERVERS_OPTS -Dip=172.31.36.107
 
 echo "==== building portal finished ===="
 
